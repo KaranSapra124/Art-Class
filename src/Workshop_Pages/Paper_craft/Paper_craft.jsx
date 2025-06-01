0874a67d0c988cdaa3b9c4461453f908985ea9e0
KaranSapra124 <sapra7112@gmail.com>
2025-06-01 15:55:55 +0530

    Added Paintings , landscape , mixed art

--- src/Workshop_Pages/Paper_craft/Paper_craft.jsx
+++ src/Workshop_Pages/Paper_craft/Paper_craft.jsx
@@ -1,54 +1,64 @@
-import React from 'react';
-import './Paper_craft.css'; 
+import React, { useEffect, useState } from 'react';
+import './Paper_craft.css';
 import { motion } from 'framer-motion';
 
-import paint1 from '../../assets/Paper_craft/c1.jpg';
-import paint2 from '../../assets/Paper_craft/c2.jpg';
-import paint3 from '../../assets/Paper_craft/c3.jpg';
-import paint4 from '../../assets/Paper_craft/c4.jpg';
-import paint5 from '../../assets/Paper_craft/c5.jpg';
-import paint6 from '../../assets/Paper_craft/c6.jpg';
-import paint7 from '../../assets/Paper_craft/c7.jpg';
-import paint8 from '../../assets/Paper_craft/c8.jpg';
-import paint9 from '../../assets/Paper_craft/c9.jpg';
+// import paint1 from '../../assets/Paper_craft/c1.jpg';
+// import paint2 from '../../assets/Paper_craft/c2.jpg';
+// import paint3 from '../../assets/Paper_craft/c3.jpg';
+// import paint4 from '../../assets/Paper_craft/c4.jpg';
+// import paint5 from '../../assets/Paper_craft/c5.jpg';
+// import paint6 from '../../assets/Paper_craft/c6.jpg';
+// import paint7 from '../../assets/Paper_craft/c7.jpg';
+// import paint8 from '../../assets/Paper_craft/c8.jpg';
+// import paint9 from '../../assets/Paper_craft/c9.jpg';
 import logogif from '../../assets/gif_logo/cr_l.gif';
+import axios from 'axios';
 
 const Paper_craft = () => {
-  const images = [
-    { id: 'image1', src: paint1, title: 'Nature’s Buzz', description: 'A cheerful honeybee flies above paper flowers, capturing the lively beauty of nature in motion.' },
-    { id: 'image2', src: paint2, title: 'Little Blooms', description: 'A charming flower pot with bright paper blooms, crafted to bring a smile and a touch of spring.' },
-    { id: 'image3', src: paint3, title: 'Elegant Silhouette', description: 'Delicate paper-cut hair forms a graceful silhouette of a woman, showcasing refined artistry.' },
-    { id: 'image4', src: paint4, title: 'Feathered Friends', description: 'A paper bird rests beside flowers, bringing a peaceful and playful touch to nature-inspired art.' },
-    { id: 'image5', src: paint5, title: 'Bag of Joy', description: 'A little boy holds a paper flower pot, adding charm and simplicity to this delightful craft.' },
-    { id: 'image6', src: paint6, title: 'Paper Treasures', description: 'A girl proudly holds a paper bag with floral details, capturing a sweet and playful moment.' },
-    { id: 'image7', src: paint7, title: 'Blooming Vase', description: 'A glass pot filled with delicate paper flowers, combining simplicity with a touch of elegance' },
-    { id: 'image8', src: paint8, title: 'Busy Bee', description: 'A honeybee holds a bucket, a playful nod to the hardworking nature of bees in a fun, creative way.' },
-    { id: 'image9', src: paint9, title: 'Teddy’s Smile', description: 'A boy with a paper-cut teddy face, a heartwarming creation full of innocence and delight.' },
-  ];
+  // const images = [
+  //   { id: 'image1', src: paint1, title: 'Nature’s Buzz', description: 'A cheerful honeybee flies above paper flowers, capturing the lively beauty of nature in motion.' },
+  //   { id: 'image2', src: paint2, title: 'Little Blooms', description: 'A charming flower pot with bright paper blooms, crafted to bring a smile and a touch of spring.' },
+  //   { id: 'image3', src: paint3, title: 'Elegant Silhouette', description: 'Delicate paper-cut hair forms a graceful silhouette of a woman, showcasing refined artistry.' },
+  //   { id: 'image4', src: paint4, title: 'Feathered Friends', description: 'A paper bird rests beside flowers, bringing a peaceful and playful touch to nature-inspired art.' },
+  //   { id: 'image5', src: paint5, title: 'Bag of Joy', description: 'A little boy holds a paper flower pot, adding charm and simplicity to this delightful craft.' },
+  //   { id: 'image6', src: paint6, title: 'Paper Treasures', description: 'A girl proudly holds a paper bag with floral details, capturing a sweet and playful moment.' },
+  //   { id: 'image7', src: paint7, title: 'Blooming Vase', description: 'A glass pot filled with delicate paper flowers, combining simplicity with a touch of elegance' },
+  //   { id: 'image8', src: paint8, title: 'Busy Bee', description: 'A honeybee holds a bucket, a playful nod to the hardworking nature of bees in a fun, creative way.' },
+  //   { id: 'image9', src: paint9, title: 'Teddy’s Smile', description: 'A boy with a paper-cut teddy face, a heartwarming creation full of innocence and delight.' },
+  // ];
+  const [images, setImages] = useState([]);
+  const fetchData = async () => {
+    const { data } = await axios.get(`${import.meta.env.VITE_Backend_url}/admin/get-workshop/Paper Craft`)
+    setImages(data?.data)
+  }
+  useEffect(() => {
 
+    fetchData()
+
+  }, [])
   return (
     <>
-        <div id="headerContainer">
-  <motion.div
-    initial={{ y: "2rem", opacity: 0 }}
-    animate={{ y: 0, opacity: 1 }}
-    transition={{
-      duration: 2,
-      type: "ease-in",
-    }}
-  >
-  
-    <span style={{ color: '#c70c0c' }}>
-    <img className='logogifsize' src={logogif}/>Expressions in
-</span>
-<span style={{color: '#f0ee84'}}> Layered Paper Forms</span>
-  </motion.div>
-</div>
+      <div id="headerContainer">
+        <motion.div
+          initial={{ y: "2rem", opacity: 0 }}
+          animate={{ y: 0, opacity: 1 }}
+          transition={{
+            duration: 2,
+            type: "ease-in",
+          }}
+        >
+
+          <span style={{ color: '#c70c0c' }}>
+            <img className='logogifsize' src={logogif} />Expressions in
+          </span>
+          <span style={{ color: '#f0ee84' }}> Layered Paper Forms</span>
+        </motion.div>
+      </div>
       <div id="galleryContainer">
-        {images.map((image, index) => (
+        {images?.length > 0 ? images.map((image, index) => (
           <motion.div
-            key={image.id}
-            id={`galleryItem-${image.id}`}
+            key={image?._id}
+            id={`galleryItem-${image?._id}`}
             className="galleryItem"
             initial={{ opacity: 0, y: 20 }}
             animate={{ opacity: 1, y: 0 }}
@@ -57,21 +67,23 @@ const Paper_craft = () => {
             whileTap={{ scale: 0.97 }}
           >
             <motion.img
-              id={`galleryImage-${image.id}`}
-              src={image.src}
-              alt={image.title}
+              id={`galleryImage-${image?._id}`}
+              src={image?.imageUrl}
+              alt={image?.title}
               className="galleryImage"
               initial={{ scale: 1 }}
               animate={{ scale: 1.05 }}
               transition={{ duration: 0.5, delay: 0.15 * index }}
             />
-            <div id={`galleryInfo-${image.id}`} className="galleryInfo">
-              <h3 id={`imageTitle-${image.id}`} className="imageTitle">{image.title}</h3>
-              <p id={`imageDescription-${image.id}`} className="imageDescription">{image.description}</p>
-              <p id={`imageAdditionalText-${image.id}`} className="imageAdditionalText">{image.additionalText}</p>
+            <div id={`galleryInfo-${image._id}`} className="galleryInfo">
+              <h3 id={`imageTitle-${image._id}`} className="imageTitle">{image?.title}</h3>
+              <p id={`imageDescription-${image?._id}`} className="imageDescription">{image?.content}</p>
+              {/* <p id={`imageAdditionalText-${image?._id}`} className="imageAdditionalText">{image.additionalText}</p> */}
             </div>
           </motion.div>
-        ))}
+        )) : <div className="spinner-border text-primary my-5" role="status">
+          {/* <span className="sr-only">Loading...</span> */}
+        </div>}
       </div>
     </>
   );
--- src/Workshop_Pages/Sculpture/Sculpture.jsx
+++ src/Workshop_Pages/Sculpture/Sculpture.jsx
@@ -1,17 +1,27 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './Sculpture.css';
 import { motion } from 'framer-motion';
 
-import quill1 from '../../assets/quiling/q1.webp';
-import quill2 from '../../assets/quiling/q2.webp';
+// import quill1 from '../../assets/quiling/q1.webp';
+// import quill2 from '../../assets/quiling/q2.webp';
 import logogif from '../../assets/gif_logo/q_l.gif';
+import axios from 'axios';
 
 const Sculpture = () => {
-  const images = [
-    { id: 'quill1', src: quill1, title: 'Floral Delight', description: "A delicate quilling art piece featuring floral patterns with vibrant colors, capturing the essence of blooming flowers." },
-    { id: 'quill2', src: quill2, title: 'Floral Delight', description: "A delicate quilling art piece featuring floral patterns with vibrant colors, capturing the essence of blooming flowers." },
-  ];
+  // const images = [
+  //   { id: 'quill1', src: quill1, title: 'Floral Delight', description: "A delicate quilling art piece featuring floral patterns with vibrant colors, capturing the essence of blooming flowers." },
+  //   { id: 'quill2', src: quill2, title: 'Floral Delight', description: "A delicate quilling art piece featuring floral patterns with vibrant colors, capturing the essence of blooming flowers." },
+  // ];
+const [images, setImages] = useState([]);
+  const fetchData = async () => {
+    const { data } = await axios.get(`${import.meta.env.VITE_Backend_url}/admin/get-workshop/Quilling`)
+    setImages(data?.data)
+  }
+  useEffect(() => {
 
+    fetchData()
+
+  }, [])
   return (
     <>
       {/* Intro Section about Quilling Art */}
@@ -31,7 +41,7 @@ const Sculpture = () => {
         >
           Discover the Art of Quilling
         </motion.h1>
-        
+
         <motion.p
           initial={{ y: "1rem", opacity: 0 }}
           animate={{ y: 0, opacity: 1 }}
@@ -65,18 +75,18 @@ const Sculpture = () => {
           }}
         >
           <span style={{ color: '#ad5631' }}>
-          <img className='logogifsize' src={logogif}/>Crafted from
+            <img className='logogifsize' src={logogif} />Crafted from
           </span>
-          <span style={{color: '#cfb240'}}> Pieces of Paper Magic</span>
+          <span style={{ color: '#cfb240' }}> Pieces of Paper Magic</span>
         </motion.div>
       </div>
 
       {/* Gallery Section */}
       <div id="galleryContainer">
-        {images.map((image, index) => (
+        {images?.length > 0 ? images.map((image, index) => (
           <motion.div
-            key={image.id}
-            id={`galleryItem-${image.id}`}
+            key={image?._id}
+            id={`galleryItem-${image?._id}`}
             className="galleryItem"
             initial={{ opacity: 0, y: 20 }}
             animate={{ opacity: 1, y: 0 }}
@@ -85,20 +95,23 @@ const Sculpture = () => {
             whileTap={{ scale: 0.97 }}
           >
             <motion.img
-              id={`galleryImage-${image.id}`}
-              src={image.src}
-              alt={image.title}
+              id={`galleryImage-${image?._id}`}
+              src={image?.imageUrl}
+              alt={image?.title}
               className="galleryImage"
               initial={{ scale: 1 }}
               animate={{ scale: 1.05 }}
               transition={{ duration: 0.5, delay: 0.15 * index }}
             />
-            <div id={`galleryInfo-${image.id}`} className="galleryInfo">
-              <h3 id={`imageTitle-${image.id}`} className="imageTitle">{image.title}</h3>
-              <p id={`imageDescription-${image.id}`} className="imageDescription">{image.description}</p>
+            <div id={`galleryInfo-${image._id}`} className="galleryInfo">
+              <h3 id={`imageTitle-${image._id}`} className="imageTitle">{image?.title}</h3>
+              <p id={`imageDescription-${image?._id}`} className="imageDescription">{image?.content}</p>
+              {/* <p id={`imageAdditionalText-${image?._id}`} className="imageAdditionalText">{image.additionalText}</p> */}
             </div>
           </motion.div>
-        ))}
+        )) : <div className="spinner-border text-primary my-5 mx-auto" role="status">
+          {/* <span className="sr-only">Loading...</span> */}
+        </div>}
       </div>
     </>
   );
